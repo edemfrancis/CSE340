3bf5264b257c2f20d87d08060bb4ce0a4f17a2d2
edemfrancis <francisedem2023@gmail.com>
2025-02-11 15:17:33 -0300

    ..

--- server.js
+++ server.js
@@ -26,19 +26,19 @@ app.set("layout", "./layouts/layout");
 /* ***********************
  * Middleware
  * ************************/
-// app
-//   .use(
-//     session({
-//       store: new (require("connect-pg-simple")(session))({
-//         createTableIfMissing: true,
-//         pool,
-//       }),
-//       secret: process.env.SESSION_SECRET,
-//       resave: true,
-//       saveUninitialized: true,
-//       name: "sessionId",
-//     })
-//   )
+app
+  .use(
+    session({
+      store: new (require("connect-pg-simple")(session))({
+        createTableIfMissing: true,
+        pool,
+      }),
+      secret: process.env.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: true,
+      name: "sessionId",
+    })
+  )
 
 // Express Messages Middleware
 app.use(require("connect-flash")());
